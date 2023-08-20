65877f3b549867cda27c565a2245451ace8c0b9a
AyushAlg08 <ayush.gupta@algo8.ai>
2023-08-20 19:13:21 +0530

    Achievements added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar()
                 <div className="menu-wrap">
                     <input type="checkbox" className="toggler" />
                     <div className="hamburger" ><div></div></div>
-                    <div className="menu" style={{overflowY:'scroll'}}>
+                    <div className="menu" style={{overflowY:'auto'}}>
                         <div>
                             <div>
                                 <ul>
--- src/pages/collab.js
+++ src/pages/collab.js
@@ -20,15 +20,15 @@ export default function Collab() {
 
               </div>
               {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
               <div class="data-collab">
                 <div class="content-text">
                   <span class="type-collab">OWASP RGIPT</span>
-                  <h1 class="title-collab"><a href="#">SIH'22 Victory</a></h1>
+                  <h1 class="title-collab"><a >SIH'22 Victory</a></h1>
                   <p class="text-collab">A remarkable achievement by our OWASP team! They emerged victorious in the prestigious Smart India Hackathon and won a cash prize of 1 lac rupees.</p>
                   <br></br>
                 </div>
@@ -44,15 +44,15 @@ export default function Collab() {
 
               </div>
               {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
               <div class="data-collab">
                 <div class="content-text">
                   <span class="type-collab">OWASP RGIPT</span>
-                  <h1 class="title-collab"><a href="#">Back-to-Back ICPC Regionals</a></h1>
+                  <h1 class="title-collab"><a >Back-to-Back ICPC Regionals</a></h1>
                   <p class="text-collab">Two of our devoted OWASP Core Members showcased their coding prowess by securing a spot in the ICPC Regionals for two consecutive years.</p>
 
                   <br />
@@ -69,16 +69,16 @@ export default function Collab() {
                 {/* <span class="year-collab">2023</span> */}
               </div>
               {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
 
               <div class="data-collab">
                 <div class="content-text">
                   <span class="type-collab">OWASP RGIPT</span>
-                  <h1 class="title-collab"><a href="#">UNESCO Hackathon</a></h1>
+                  <h1 class="title-collab"><a >UNESCO Hackathon</a></h1>
                   <p class="text-collab">Demonstrating their exceptional skills, two members of our core team were chosen as finalists for the prestigious UNESCO: India-Africa Hackathon, organized by the GOI.
                   </p>
                   <br />
@@ -95,15 +95,15 @@ export default function Collab() {
                 {/* <span class="year-collab">2023</span> */}
               </div>
               {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
               <div class="data-collab">
                 <div class="content-text">
                   <span class="type-collab">OWASP RGIPT</span>
-                  <h1 class="title-collab"><a href="#">ET Campus Star 2023 Honoree</a></h1>
+                  <h1 class="title-collab"><a >ET Campus Star 2023 Honoree</a></h1>
                   <p class="text-collab">One of our dedicated core members has secured a place of honor in the esteemed ET Campus Star 2023 list, a testament to their exceptional commitment and talent.</p>
 
                   <br />
@@ -132,15 +132,15 @@ export default function Collab() {
 
               </div>
               {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
               <div class="data-collab">
                 <div class="content-text">
                   <span class="type-collab">OWASP RGIPT</span>
-                  <h1 class="title-collab"><a href="#">Paper Accepted at RAIT 2023</a></h1>
+                  <h1 class="title-collab"><a >Paper Accepted at RAIT 2023</a></h1>
                   <p class="text-collab">Celebrate with us as our core team member's paper on "Blockchain in Healthcare" secures acceptance at RAIT 2023, highlighting their commitment to innovative research.</p>
 
                   <br></br>
@@ -148,59 +148,61 @@ export default function Collab() {
                 </div>
               </div>
             </div>
-            {/* </div> */}
+            </div>
 
-            {/* <div class="final-6 card-collab">
+            <div class="final-6 card-collab">
     <div class="wrapper-collab">
       <div class="date-collab">
-        <span class="day-collab">Sep</span>
-        <span class="month-collab">2022</span>
+        <span class="day-collab">Jun</span>
+        <span class="month-collab">2023</span>
        
-      </div> */}
+     </div> 
             {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
-            {/* <div class="data-collab">
+            <div class="data-collab">
         <div class="content-text">
           <span class="type-collab">OWASP RGIPT</span>
-          <h1 class="title-collab"><a href="#">Back-to-Back ICPC Regionals</a></h1>
-<p class="text-collab">Two of our devoted OWASP Core Members showcased their coding prowess by securing a spot in the ICPC Regionals for two consecutive years.</p>
+          <h1 class="title-collab"><a >IDE Bootcamp 2023 Victors</a></h1>
+<p class="text-collab">Cheers to our accomplished core team members who emerged as winners in the prestigious IDE Bootcamp 2023, hosted by AICTE and MoE. Their victory reflects their dedication and expertise.</p>
 
+
+          <br/>
           <br/>
 
         </div>
       </div>
     </div>
-  </div> */}
-            {/* <div class="final-7 card-collab">
+  </div>
+            <div class="final-7 card-collab">
     <div class="wrapper-collab">
       <div class="date-collab">
-      <span class="day-collab">Dec</span>
-        <span class="month-collab">2022</span> */}
-            {/* <span class="year-collab">2023</span> */}
+      <span class="day-collab">Nov</span>
+        <span class="month-collab">2022</span> 
+        </div>
+            {/* <span class="year-collab">2023</span>
           </div>
           {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
 
-          {/* <div class="data-collab">
+          <div class="data-collab">
         <div class="content-text">
         <span class="type-collab">OWASP RGIPT</span>
-          <h1 class="title-collab"><a href="#">UNESCO Hackathon Finalists</a></h1>
-<p class="text-collab">Demonstrating their exceptional skills, two members of our core team were chosen as finalists for the prestigious UNESCO: India-Africa Hackathon, organized by the GOI. 
-</p>
+        <h1 class="title-collab"><a >Cartesi Project Grant Recipients</a></h1>
+       <p class="text-collab">Congratulations to our team for securing the esteemed Cartesi Project Grant, enabling them to work on the development of "The Blockchain OS."</p>
           <br/>
 
         </div>
       </div>
     </div>
-  </div> */}
+  </div>
           <div class="final-8 card-collab">
             <div class="wrapper-collab">
               <div class="date-collab">
@@ -209,15 +211,15 @@ export default function Collab() {
                 {/* <span class="year-collab">2023</span> */}
               </div>
               {/* <ul class="menu-content-collab">
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-html5"></i></a></li>
-          <li><a href="#"><i class="fab fa-css3-alt"></i></a></li>
-          <li><a href="#"><i class="fab fa-js"></i></a></li>
+          <li><a ><i class="fab fa-github"></i></a></li>
+          <li><a ><i class="fab fa-html5"></i></a></li>
+          <li><a ><i class="fab fa-css3-alt"></i></a></li>
+          <li><a ><i class="fab fa-js"></i></a></li>
       </ul> */}
               <div class="data-collab">
                 <div class="content-text">
                   <span class="type-collab">OWASP RGIPT</span>
-                  <h1 class="title-collab"><a href="#">Amazon ML Summer School Selection</a></h1>
+                  <h1 class="title-collab"><a >Amazon ML Summer School Selection</a></h1>
                   <p class="text-collab">Stepping into excellence: a core team member has been handpicked for the prestigious Amazon ML Summer School, a clear recognition of their outstanding skills and potential.</p>
 
 
